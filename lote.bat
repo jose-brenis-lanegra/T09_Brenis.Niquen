21f292120244b33496d71dcefeb40c6c8a5b4490
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-12-17 22:37:34 -0500

    Create lote.bat

---
+++ lote.bat
@@ -0,0 +1,75 @@
+python problema01.py 324 387
+python problema01.py 145 468
+python problema01.py 245 24
+python problema02.py 34
+python problema02.py 7
+python problema02.py 24
+python problema03.py 35 96
+python problema03.py 255 6
+python problema03.py 75 134
+python problema04.py 3 9
+python problema04.py 78 307
+python problema04.py 54 87
+python problema05.py 362 4826 43
+python problema05.py 20 95 35
+python problema05.py 1 57 298
+python problema06.py 49
+python problema06.py 93
+python problema06.py 24
+python problema07.py 3.14 8
+python problema07.py 3.14 274
+python problema07.py 3.14 305
+python problema08.py 354 8463
+python problema08.py 27 49
+python problema08.py 195 25
+python problema09.py 28 405 26 36
+python problema09.py 18 56 18 56
+python problema09.py 38 38 38 38
+python problema10.py 76
+python problema10.py 24
+python problema10.py 11
+python problema11.py 46 478 18
+python problema11.py 84 29 35
+python problema11.py 14 73 40
+python problema12.py 3.14 652
+python problema12.py 3.14 923
+python problema12.py 3.14 365
+python problema13.py 3.14 23
+python problema13.py 3.14 47
+python problema13.py 3.14 89
+python problema14.py 3.14 37 58
+python problema14.py 3.14 56 17
+python problema14.py 3.14 34 29
+python problema15.py 27 13
+python problema15.py 4 3
+python problema15.py 100 50
+python problema16.py 32
+python problema16.py 43
+python problema16.py 67
+python problema17.py 535
+python problema17.py 321
+python problema17.py 846
+python problema18.py 36
+python problema18.py 57
+python problema18.py 45
+python problema19.py 78
+python problema19.py 265
+python problema19.py 12
+python problema20.py 27 9 52
+python problema20.py 86 349 36
+python problema20.py 18 20 6
+python problema21.py "ANITALAVALATINA"
+python problema21.py "POR DONDE SERA"
+python problema21.py "PEPEP"
+python problema22.py "jose" "juan"
+python problema22.py "maria" "magdalena"
+python problema22.py "abel" "cain"
+python problema23.py "me mata tu mirada"
+python problema23.py "no sabes nada del amor"
+python problema23.py "el mejor del mundo"
+python problema24.py "tu misma lo haces"
+python problema24.py "no seas asi Vania"
+python problema24.py "porque seran asi los Varones"
+python problema25.py "sos lo mejor que me ha pasado"
+python problema25.py "adios , buenas noches"
+python problema25.py "has caso a lo que te dija"
