4084a8a00ae673f192dba9b9892dd54ea47cc532
jose-brenis-lanegra <57306291+jose-brenis-lanegra@users.noreply.github.com>
2019-12-17 22:23:25 -0500

    Create problema12.py

---
+++ problema12.py
@@ -0,0 +1,9 @@
+import libreria
+import os
+
+#demostrar si el volumen de una esfera esta correcto
+pi=float(os.sys.argv[1])
+r=int(os.sys.argv[2])
+
+demostrar=libreria.demostrar_esfera(pi,r)
+print("el volumen de esta esfera esta:", demostrar)
